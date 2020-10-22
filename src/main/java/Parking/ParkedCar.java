7ea2edf93ae49f409efd8531864bcae601833f65
John Kuefler <john@dev-squared.com>
2020-10-22 12:00:12 -0500

    initial commit

---
+++ src/main/java/Parking/ParkedCar.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Parking;
+
+/**
+ *
+ * @author johnk
+ */
+public class ParkedCar {
+    private String make;
+    private String model;
+    private String color;
+    private String licenseNumber;
+    private int numberOfMinutesParked;
+
+    public ParkedCar(String make, String model, String color, String licenseNumber, int numberOfMinutesParked) {
+        this.make = make;
+        this.model = model;
+        this.color = color;
+        this.licenseNumber = licenseNumber;
+        this.numberOfMinutesParked = numberOfMinutesParked;
+    }
+
+    public ParkedCar(ParkedCar car2) {
+        this.make = car2.make;
+        this.model = car2.model;
+        this.color = car2.color;
+        this.licenseNumber = car2.licenseNumber;
+        this.numberOfMinutesParked = car2.numberOfMinutesParked;
+    }
+    
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getLicenseNumber() {
+        return licenseNumber;
+    }
+
+    public void setLicenseNumber(String licenseNumber) {
+        this.licenseNumber = licenseNumber;
+    }
+
+    public int getNumberOfMinutesParked() {
+        return numberOfMinutesParked;
+    }
+
+    public void setNumberOfMinutesParked(int numberOfMinutesParked) {
+        this.numberOfMinutesParked = numberOfMinutesParked;
+    }
+
+    @Override
+    public String toString() {
+        return "ParkedCar{" + "make=" + make + ", model=" + model + ", color=" + color + ", licenseNumber=" + licenseNumber + ", numberOfMinutesParked=" + numberOfMinutesParked + '}';
+    }
+    
+}
